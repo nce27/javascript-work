1795f0993a4e0f828f67606f044002738e432f09
nce27 <nc3elliott@gmail.com>
2023-06-15 17:09:49 +0100

    condition controlled loop for assigning odd and even numbers

---
+++ 05_conditionals/loops.js
@@ -0,0 +1,20 @@
+// condition controlled loops
+
+// for (let i = 0 ; i < 10 ; i++){
+//   console.log(i)
+// }
+
+
+// let counter = 0;
+// while (counter <= 10){
+//   console.log(counter)
+//   counter++;
+// }
+
+for (let x = 0 ; x <= 20 ; x++) {
+  if (x % 2 === 0){
+    console.log(`${x} is even`)
+  } else {
+    console.log(`${x} is odd`)
+  }
+}
